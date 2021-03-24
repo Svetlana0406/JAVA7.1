8edb60bc828a67cc85181c61b541164b4366b8bb
Svetlana <svetlana.samrinova@yandex.ru>
2021-03-24 22:50:00 +0300

    update maven.yml + @Test

--- src/test/java/statistic/StatisticsServiceTest.java
+++ src/test/java/statistic/StatisticsServiceTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StatisticsServiceTest {
-    //@Test
+    @Test
     void findMax() {
         StatisticsService service = new StatisticsService();
 
